72c734cdebebc2e2156e257e2cc9b165cd8e27aa
XiaoXiangJinLan <2855315414@qq.com>
2025-12-19 14:18:58 +0800

    创建1.20.1-fabric分支

---
+++ src/main/java/com/jinlan/moreornplants/More_Orn_PlantsDataGenerator.java
@@ -0,0 +1,11 @@
+package com.jinlan.moreornplants;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class More_Orn_PlantsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
