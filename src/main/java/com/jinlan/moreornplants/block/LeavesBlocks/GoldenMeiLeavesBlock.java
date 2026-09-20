762a49f50c1d50584d568f060a526580904e27ab
XiaoXiangJinLan <2855315414@qq.com>
2026-09-21 00:32:48 +0800

    修改了青梅纹理、模型； 修改了仙桃模型； 修改了桃木剑纹理； 修改了梅花树苗纹理； 修改了江梅结果逻辑

--- src/main/java/com/jinlan/moreornplants/block/LeavesBlocks/GoldenMeiLeavesBlock.java
+++ src/main/java/com/jinlan/moreornplants/block/LeavesBlocks/GoldenMeiLeavesBlock.java
@@ -27,7 +27,8 @@ public class GoldenMeiLeavesBlock extends LeavesBlock {
 
         if (!level.isAreaLoaded(pos, 1)) return;
 
-        if (!state.getValue(BlockStateProperties.PERSISTENT)) {
+        int distance = state.getValue(BlockStateProperties.DISTANCE);
+        if (!state.getValue(BlockStateProperties.PERSISTENT) && (distance == 3 || distance == 5)) {
             BlockPos belowPos = pos.below();
             if (level.isEmptyBlock(belowPos)) {
                 int chance = level.getBiome(pos).is(ModTags.Biomes.APRICOT_BEARING) ? 5 : 10;
